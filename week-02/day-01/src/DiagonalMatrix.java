e76d1d85194f32f6c102dd34699e27f7138bbf93
Ondrej Ivanko <woshi4ndy@gmail.com>
2019-04-08 14:16:45 +0200

    AppendA task done.

--- week-02/day-01/src/DiagonalMatrix.java
+++ week-02/day-01/src/DiagonalMatrix.java
@@ -1,11 +1,11 @@
 public class DiagonalMatrix {
     public static void main(String[] args) {
-        int[][] array =  {{1, 0}, {0, 0}};
+       /* int[][]
         for (int i = 0; i < array.length * 2; i++) {
             System.out.println(array[0][1]+array[0][0]);
 
 
-        }
+        } */
 
 
     }
