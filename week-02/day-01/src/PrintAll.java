4b7df2c521fa3365bced9c952585372470309724
Ondrej Ivanko <woshi4ndy@gmail.com>
2019-04-08 13:35:14 +0200

    PrintAll Task done.

---
+++ week-02/day-01/src/PrintAll.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class PrintAll {
+    public static void main(String[] args) {
+        int[] numbers = {4, 5, 6, 7};
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+    // - Create an array variable named `numbers`
+    //   with the following content: `[4, 5, 6, 7]`
+    // - Print all the elements of `numbers`
+}
