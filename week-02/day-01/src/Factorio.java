0a459b81acd1bf9f15eccfdaeb082801299f7c41
Ondrej Ivanko <woshi4ndy@gmail.com>
2019-04-08 13:02:53 +0200

    Factorio Task completed

---
+++ week-02/day-01/src/Factorio.java
@@ -0,0 +1,16 @@
+public class Factorio {
+    public static void main(String[] args) {
+        System.out.println(factorio(4));
+    }
+
+    public static int factorio(int number) {
+        int factorial = 1;
+        for (int i = 0; i < number; i++) {
+            factorial *= (number - i);
+        }
+        return factorial;
+    }
+
+    // - Create a function called `factorio`
+    //   that returns it's input's factorial
+}
