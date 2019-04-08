6db730b4f64eb39378b24f1d25948e7bf7ba7fa3
Ondrej Ivanko <woshi4ndy@gmail.com>
2019-04-08 11:54:39 +0200

    Doubling Task completed

---
+++ week-02/day-01/src/Doubling.java
@@ -0,0 +1,15 @@
+public class Doubling {
+    public static void main(String[] args) {
+        int baseNum = 123;
+        System.out.println(doubling(baseNum));
+    }
+
+    public static int doubling(int num) {
+        int result = num * 2;
+        return result;
+    }
+// - Create an integer variable named `baseNum` and assign the value `123` to it
+// - Create a function called `doubling` that doubles it's input parameter and returns with an integer
+// - Print the result of `doubling(baseNum)`
+
+}
