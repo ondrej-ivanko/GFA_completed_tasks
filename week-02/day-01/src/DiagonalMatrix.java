d8232ca4185e6f06ae8745f29cee20e30a25e361
Ondrej Ivanko <woshi4ndy@gmail.com>
2019-04-08 14:05:44 +0200

    DoubleItems task done.

---
+++ week-02/day-01/src/DiagonalMatrix.java
@@ -0,0 +1,24 @@
+public class DiagonalMatrix {
+    public static void main(String[] args) {
+        int[][] array =  {{1, 0}, {0, 0}};
+        for (int i = 0; i < array.length * 2; i++) {
+            System.out.println(array[0][1]+array[0][0]);
+
+
+        }
+
+
+    }
+
+
+}
+    // - Create (dynamically) a two dimensional array
+    //   with the following matrix. Use a loop!
+    //
+    //   1 0 0 0
+    //   0 1 0 0
+    //   0 0 1 0
+    //   0 0 0 1
+    //
+    // - Print this two dimensional array to the output
+
