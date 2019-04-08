8a271c1a2993f4a50376e2f330f2ba010bc6cde6
Ondrej Ivanko <woshi4ndy@gmail.com>
2019-04-08 14:36:13 +0200

    Reverse Task done.

---
+++ week-02/day-01/src/Reverse.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Reverse {
+    public static void main(String[] args) {
+        int[] aj = {3, 4, 5, 6, 7};
+        for (int i = 0; i < aj.length/2; i++) {
+            int temp_var = aj[i];
+            aj[i] = aj[aj.length - 1 - i];
+            aj[aj.length - 1 - i] = temp_var;
+        }
+        System.out.println(Arrays.toString(aj));
+    }
+    // - Create an array variable named `aj`
+    //   with the following content: `[3, 4, 5, 6, 7]`
+    // - Reverse the order of the elements in `aj`
+    // - Print the elements of the reversed `aj`
+}
