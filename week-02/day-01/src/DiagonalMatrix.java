77c9e22aa803ff2487057e12feca05558958ca30
Ondrej Ivanko <woshi4ndy@gmail.com>
2019-04-08 21:29:43 +0200

    DiagonalMatrix Task solved.

--- week-02/day-01/src/DiagonalMatrix.java
+++ week-02/day-01/src/DiagonalMatrix.java
@@ -4,24 +4,12 @@ public class DiagonalMatrix {
     public static void main(String[] args) {
         int[][] array = new int[4][4];
         for (int[] smallerArray : array) {
-            int idx = 0;
-            for (int y = 0; y < smallerArray.length; y++) {
-                if (idx == y) smallerArray[y] = 1;
-                else smallerArray[y] = 0;
-                }
-            }
-        idx += 1;
-
             for (int i = 0; i < smallerArray.length; i++) {
-                System.out.println(smallerArray[i]);
+                array [i][i] = 1;
+                }
+            System.out.println(Arrays.toString(smallerArray));
             }
-
         }
-
-
-
-    }
-
 
 }
     // - Create (dynamically) a two dimensional array
