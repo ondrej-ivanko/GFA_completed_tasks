c32b55c75b83164f5add3cd7f2006ce67e509ed5
Ondrej Ivanko <woshi4ndy@gmail.com>
2019-04-09 09:37:06 +0200

    Palindrome Task done.

--- week-02/day-01/src/Anagram.java
+++ week-02/day-01/src/Anagram.java
@@ -1,8 +1,8 @@
 public class Anagram {
-    public static void main(String[] args) {
-        String word = "Horse";
-    }
-    public static String anagram(String[] word) {
-        return "none";
-    }
+        public static void main(String[] args) {
+            String word = "Horse";
+        }
+        public static String anagram(String[] word) {
+            return "none";
+        }
 }
--- week-02/day-01/src/Palindrome.java
+++ week-02/day-01/src/Palindrome.java
@@ -2,24 +2,24 @@ import java.util.Arrays;
 
 public class Palindrome {
     public static void main(String[] args) {
-        String word = "Neexistuje";
-
-        System.out.println();
-    }/*
+        String word = "Hello";
+        String strFnc = palindrome(word);
+        System.out.println(strFnc);
+    }
     public static String palindrome(String someWord) {
         char[] inputedString = someWord.toCharArray();
         char[] palindromeArray = new char[inputedString.length];
         String finalString = "";
-        for (int i = 0; i < inputedString.length; i--) {
-            palindromeArray[i] = inputedString[inputedString.length - 1 - i];
+        for (int i = 0; i < palindromeArray.length; i++) {
+            finalString += inputedString[i];
+        }
+        for (int i = 0; i < inputedString.length; i++) {
+            finalString += inputedString[inputedString.length - 1 - i];
 
         }
-        for (int i = 0; i <= palindromeArray.length * 2; i++) {
-            finalString += inputedString[i];
-            finalString += palindromeArray[i];
-        }
+
         return finalString;
-    }*/
+    }
 
 
 }
