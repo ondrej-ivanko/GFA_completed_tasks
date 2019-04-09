aa0aa14b979a2ccf5ad8938e274a2011fef243f2
Ondrej Ivanko <woshi4ndy@gmail.com>
2019-04-09 13:22:10 +0200

    Offered 3 solutions for UrlFixer Task.

--- week-02/day-02/src/UrlFixer.java
+++ week-02/day-02/src/UrlFixer.java
@@ -1,3 +1,5 @@
+import com.sun.xml.internal.fastinfoset.util.CharArray;
+
 public class UrlFixer {
     public static void main(String... args){
         String url = "https//www.reddit.com/r/nevertellmethebots";
@@ -9,6 +11,24 @@ public class UrlFixer {
         url = url.replace("bots", "odds").replace("//", "://");
         System.out.println(url);
         //Second solution:
-        
+        String subString1 = url.substring(0,5).concat(":");
+        String subString2 = url.substring(5, url.length()-4).concat("odds");
+        String desiredUrl = subString1.concat(subString2);
+        System.out.println(desiredUrl);
+        // Third solution:
+        char[] charedString = url.toCharArray();
+        String correctUrl = "";
+        for (int i = 0; i < url.length(); i++) {
+            if (i == 5) {
+                correctUrl += ":";
+                correctUrl += charedString[i];
+            } else if (i == url.length()-4) {
+                correctUrl += "odds";
+            }
+            else {
+                correctUrl += charedString[i];
+            }
+        }
+        System.out.println(correctUrl); */
     }
 }
