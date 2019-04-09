76eb89cf22c04a655d5566d6a40fb52dd4ce1757
Ondrej Ivanko <woshi4ndy@gmail.com>
2019-04-09 14:17:46 +0200

    ListIntroduction1 Task done.

---
+++ week-02/day-02/src/ListIntroduction1.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class ListIntroduction1 {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+        System.out.println(names);
+        names.add("William");
+        boolean i = names.isEmpty();
+        System.out.println(i);
+        names.add("John");
+        names.add("Amanda");
+        System.out.println(names.size());
+        System.out.println(names.get(2));
+        for (String name : names) {
+            System.out.println(name);
+        }
+        for (int j = 0; j < names.size(); j++) {
+            System.out.println((j + 1) + ". " + names.get(j) );
+        }
+        //or second possibility:
+        int idx = 1;
+        for (String name : names) {
+            System.out.println(idx + ". " + name);
+            idx++;
+        }
+        names.remove(1);
+
+        for (int j = names.size() - 1; j >= 0; j--) {
+            System.out.println(names.get(j));
+        }
+        names.clear();
+    }
+
+   // public static void () {
+
+    //}
+}
