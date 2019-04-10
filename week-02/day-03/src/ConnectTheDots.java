def6c056576e3f69a4619b0ad55f780c67774433
Ondrej Ivanko <woshi4ndy@gmail.com>
2019-04-10 17:47:50 +0200

    Some excercises finished.

---
+++ week-02/day-03/src/ConnectTheDots.java
@@ -0,0 +1,55 @@
+import javax.swing.*;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class ConnectTheDots {
+    public static void mainDraw(Graphics graphics) {
+        // Create a function that takes 2 parameters:
+        // An array of {x, y} points and graphics
+        // and connects them with green lines.
+        // Connect these to get a box: {{10, 10}, {290,  10}, {290, 290}, {10, 290}}
+        // Connect these: {{50, 100}, {70, 70}, {80, 90}, {90, 90}, {100, 70},
+        // {120, 100}, {85, 130}, {50, 100}}
+        int[][] newArray = {{10, 10}, {290,  10}, {290, 290}, {10, 290}};
+        int[][] secondArray = {{50, 100}, {70, 70}, {80, 90}, {90, 90}, {100, 70}, {120, 100}, {85, 130}, {50, 100}};
+        connectDots(newArray, graphics);
+
+
+    }
+    public static void connectDots(int[][] xArray, Graphics graphics) {
+        ArrayList<Integer> coordArray = new ArrayList<>();
+        for (int[] array : xArray) {
+            for (int element : array
+                 ) {
+                coordArray.add(element);
+                }
+            }
+        graphics.drawLine(coordArray[1], coordArray[2], coordArray[3], coordArray[4]);
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
---
+++ week-02/day-03/src/FunctionToCenter.java
@@ -0,0 +1,53 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class FunctionToCenter {
+    public static void mainDraw(Graphics graphics) {
+        // Create a line drawing function that takes 3 parameters:
+        // The x and y coordinates of the line's starting point and the graphics
+        // and draws a line from that point to the center of the canvas.
+        // Fill the canvas with lines from the edges, every 20 px, to the center.
+        int linesDistance = 20;
+        int incrementOfCoordinates = HEIGHT / linesDistance;
+        int x = 0;
+        int y = 0;
+        for (int i = 0; i <= 4 * incrementOfCoordinates; i++) {
+            writeLine(x, y, graphics);
+            if (i <= incrementOfCoordinates + 3) {y += incrementOfCoordinates;}
+            else if (i <= incrementOfCoordinates * 2) {x += incrementOfCoordinates;}
+            else if (i <= incrementOfCoordinates * 3) {y -= incrementOfCoordinates;}
+            else if (i <= incrementOfCoordinates * 4) {x -= incrementOfCoordinates;}
+
+        }
+    }
+
+    public static void writeLine(int xCoord, int yCoord, Graphics graphics) {
+        graphics.drawLine(xCoord, yCoord, HEIGHT / 2, WIDTH / 2);
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
