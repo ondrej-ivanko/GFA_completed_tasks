7ba3292b580c731d4f98804fc502997788417345
Ondrej Ivanko <woshi4ndy@gmail.com>
2019-04-11 15:26:05 +0200

    MaximumFinder Task done.

---
+++ week-02/day-04/src/MaximumFinder.java
@@ -0,0 +1,17 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MaximumFinder {
+    public static void main(String[] args) {
+        int[] array = {24, 56, 22051, 989, 61, 99, 1850, 415};
+        System.out.println(maxNumber((array), array.length - 1, 0));
+    }
+
+    public static int maxNumber(int[] myArray, int count, int max) {
+        if (count < 1) return max;
+        if (myArray[count] > max) max = myArray[count];
+        return maxNumber(myArray, count - 1, max);
+
+    }
+}
