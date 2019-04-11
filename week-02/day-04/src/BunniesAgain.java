fa3d6a5117239098f9d6b01a2785f2c0098a3f95
Ondrej Ivanko <woshi4ndy@gmail.com>
2019-04-11 14:04:23 +0200

    bunnies Again task & String task done.

---
+++ week-02/day-04/src/BunniesAgain.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class BunniesAgain {
+    public static void main(String[] args) {
+        ArrayList<Integer> bunnyLine = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8));
+        int earsTwo = 2;
+        int earsThree = 3;
+        System.out.println(earsCount(bunnyLine.size() - 1, earsTwo, earsThree));
+    }
+
+    public static int earsCount(int bunnies, int earsOdd ,int earsEven) {
+        if (bunnies <= 1) {return earsOdd + earsEven;}
+        if (bunnies % 2 == 0) {return earsEven + earsCount(bunnies - 1, earsOdd, earsEven);}
+        else {return earsOdd + earsCount(bunnies - 1, earsOdd, earsEven);}
+    }
+}
---
+++ week-02/day-04/src/Strings.java
@@ -0,0 +1,14 @@
+public class Strings {
+    public static void main(String[] args) {
+        String inputed = "XsXXxxxyyyioklk hlkf hlk!%0 jdlkfjxYYx";
+        System.out.println(stringSwap(inputed, inputed.length() - 1));
+    }
+
+    public static String stringSwap(String sentence, int count) {
+        if (count <= 0) return sentence;
+        String character = sentence.substring(count, count + 1);
+        if ("x".equals(character)) {sentence = sentence.replace(character, "y");}
+        return stringSwap(sentence, count - 1);
+
+    }
+}
