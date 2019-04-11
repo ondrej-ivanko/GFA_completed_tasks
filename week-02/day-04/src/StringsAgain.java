dee6a44e461a3986778b3070328f1030b21157f3
Ondrej Ivanko <woshi4ndy@gmail.com>
2019-04-11 14:18:36 +0200

    StringsAgain Task done.

---
+++ week-02/day-04/src/StringsAgain.java
@@ -0,0 +1,13 @@
+public class StringsAgain {
+    public static void main(String[] args) {
+        String inputed = "XsXXxxxyyyioklk hlkf hlk!%0 jdlkfjxYYx";
+        System.out.println(stringAgain(inputed, inputed.length() - 1));
+    }
+
+    public static String stringAgain(String sentence, int count) {
+        if (count <= 0) return sentence;
+        String character = sentence.substring(count, count + 1);
+        if ("x".equals(character)) {sentence = sentence.substring(0, count).concat(sentence.substring(count + 1));}
+        return stringAgain(sentence, count - 1);
+    }
+}
