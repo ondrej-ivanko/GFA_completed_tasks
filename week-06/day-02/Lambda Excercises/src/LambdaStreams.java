7a59bce7d9ca3a5fa71ee3701024b1c507d6200b
Ondrej Ivanko <woshi4ndy@gmail.com>
2019-05-14 21:55:42 +0200

    New text file created and code commented.

--- week-06/day-02/Lambda Excercises/src/LambdaStreams.java
+++ week-06/day-02/Lambda Excercises/src/LambdaStreams.java
@@ -17,50 +17,60 @@ public class LambdaStreams {
 
 	public static void main(String[] args) {
 
-
-		/*numbers.stream()
+		// Every even number
+		numbers.stream()
 				.filter(num -> num % 2 == 0)
 				.forEachOrdered(System.out::println);
 
+		// Every even positive num squared
 		numbers.stream()
 				.filter(num -> num % 2 == 0)
 				.filter(num -> num > 0)
 				.map(num -> (int)(Math.pow(num, 2)))
 				.forEach(System.out::println);
-*/
-		/*numbers2.stream()
+
+		// Every every num that after squaring is > 20
+		numbers2.stream()
 				.map(num -> (int)(Math.pow(num, 2)))
 				.filter(num -> num > 20)
-				.forEach(System.out::println);*/
+				.forEach(System.out::println);
 
-		/*double average = numbers.stream()
+		// Sum of all even nums averaged
+		double average = numbers.stream()
 				                .filter(num -> num % 2 != 0)
 				                .mapToDouble(num -> num).average().getAsDouble();
-		System.out.println(average);*/
+		System.out.println(average);
 
-		/*System.out.println(numbers3.stream()
-				.mapToInt(num -> num).sum());*/
+		// Sum of all nums.
+		System.out.println(numbers3.stream()
+				.mapToInt(num -> num).sum());
 
-		/*List<String> lesscities = cities.stream()
+		// Every citys name starting with "Z".
+		List<String> lesscities = cities.stream()
 								 .filter(city -> city.startsWith("Z"))
 								 .collect(toList());
-		System.out.println(lesscities);*/
+		System.out.println(lesscities);
 
-		/*word.chars()
+		// Check if letter is upperCased.
+		word.chars()
 				.filter(Character::isUpperCase);
 
-		word.chars()
+		// Removes all non-upperCase letters from String and returns the new string.
+		String clearWord = word.chars()
 				.filter(Character::isUpperCase)
-				.mapToObj(character -> String.valueOf(character))
+				.mapToObj(character -> String.valueOf((char)character))
 				.collect(joining());
+		System.out.println(clearWord);
 
+		// Returns letter occurrence in String
 		Map<String, Long> frequentChars = word.chars()
 				.mapToObj(c -> String.valueOf((char) c))
 				.collect(Collectors.groupingBy(c -> c, Collectors.counting()));
 
+		// Loops thorough previous map
 		for (Map.Entry<String, Long> letters : frequentChars.entrySet()) {
 			System.out.print(letters.getKey() + "=" + letters.getValue() + ", ");
-		}*/
+		}
 
 		List<Fox> container = new ArrayList<>();
 		container.add(new Fox("Denny", "green", 4));
@@ -69,23 +79,26 @@ public class LambdaStreams {
 		container.add(new Fox("jjk", "brown", 19));
 		container.add(new Fox("nvb", "cyan", 35));
 
+		// Returns name of foxes that are green
 		List<String> greenfoxes = container.stream()
 				.filter(fox -> fox.getColor() == "green")
 				.map(fox -> fox.getName())
 				.collect(toList());
 
-
 		System.out.println(greenfoxes);
 
+		// Returns names of foxes that are green and younger than 5
 		List<String> greenfoxes2 = container.stream()
 				                          .filter(fox -> fox.getColor() == "green" && fox.getAge() < 5)
 				                          .map(fox -> fox.getName())
 				                          .collect(toList());
 		System.out.println(greenfoxes2);
 
+		// Returns colour occurrence of foxes as int.
 		Map<String, Long> foxesByColour = container.stream()
 					.map(fox -> fox.getColor())
 				    .collect(Collectors.groupingBy(fox -> fox, Collectors.counting()));
+		
 		System.out.println(foxesByColour);
 
 
