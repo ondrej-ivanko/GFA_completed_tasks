5368b47a5b15906c8cb273b61a265d9d6f4945ac
Ondrej Ivanko <woshi4ndy@gmail.com>
2019-05-15 15:32:36 +0200

    HelloWebController introduced and all tasks solved.

--- week-06/day-03/Demo For SpringBoot/src/main/java/com/greenfoxacademy/springstart/controllers/Greeting.java
+++ week-06/day-03/Demo For SpringBoot/src/main/java/com/greenfoxacademy/springstart/controllers/Greeting.java
@@ -1,19 +1,41 @@
 package com.greenfoxacademy.springstart.controllers;
 
-public class Greeting {
-	long id;
-	String content;
+import java.util.concurrent.atomic.AtomicLong;
 
-	public Greeting(long id, String content) {
-		this.id = id;
+public class Greeting {
+
+	private static AtomicLong id = new AtomicLong();
+	String content;
+	static String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+			"Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+			"Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+			"Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+
+	public Greeting(String content) {
 		this.content = content;
+		id.incrementAndGet();
 	}
 
-	public long getId() {
+	public String loopThroughHellos() {
+		for (String hello : hellos) {
+			return hello;
+		}
+		return null;
+	}
+
+	public String[] getHellos() {
+		return hellos;
+	}
+
+	public AtomicLong getId() {
 		return id;
 	}
 
 	public String getContent() {
 		return content;
+	}
+
+	public String toString() {
+		return getHellos().toString();
 	}
 }
--- week-06/day-03/Demo For SpringBoot/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-06/day-03/Demo For SpringBoot/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -5,13 +5,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import static oracle.jrockit.jfr.events.Bits.intValue;
+
+
 @RestController
 public class HelloRESTController {
 
 	@RequestMapping(value="/greeting")
 	public @ResponseBody String hello(@RequestParam String name) {
-		Greeting greeting = new Greeting(1, "Hello World!");
-		return "{ id: " + greeting.getId() + ", " + "content: " + greeting.getContent() + ", " + name + " }";
+		Greeting greeting = new Greeting("Hello World!");
+			return greeting.getHellos()[intValue(greeting.getId()) - 1] + " " + name + "! This site was loaded " + greeting.getId()
+					       + " times since last server start.";
 	}
 
 }
